4382fd2da3a42aecc24b66389e3e595abd3f0fd3
rankr66 <125305290+rankr66@users.noreply.github.com>
2024-01-22 00:37:39 +0530

    changes

--- app.js
+++ app.js
@@ -15,10 +15,14 @@ const flash = require("connect-flash");
 const bcrypt = require("bcrypt");
 const cors = require("cors");
 const multer = require("multer");
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://collaborative-blogging.onrender.com'],
+    credentials: true,
+    exposedHeaders: ["Access-Control-Allow-Headers", "Access-Control-Allow-Methods"],
+  })
+);
+
 app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false }));
