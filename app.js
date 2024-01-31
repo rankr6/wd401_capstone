de81004048a77eb76db6105f75fd630cd5595ac7
rankr66 <125305290+rankr66@users.noreply.github.com>
2024-02-01 02:55:17 +0530

    changes

--- app.js
+++ app.js
@@ -22,7 +22,11 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
